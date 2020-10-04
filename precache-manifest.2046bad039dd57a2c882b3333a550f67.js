7644b5c26a2413a9a0c93d33798f07b3e6c7d054
dksx <37441849+dksx@users.noreply.github.com>
2020-10-04 03:09:08 +0200

    Updates

---
+++ precache-manifest.2046bad039dd57a2c882b3333a550f67.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcbaac500c30a7103ca06cd492c55f8e",
+    "url": "/okta-vidispine/index.html"
+  },
+  {
+    "revision": "62c6429764c49dbccdbc",
+    "url": "/okta-vidispine/static/css/2.f9b8cb33.chunk.css"
+  },
+  {
+    "revision": "7fec02a902fdcc96a921",
+    "url": "/okta-vidispine/static/css/main.29f06e3a.chunk.css"
+  },
+  {
+    "revision": "62c6429764c49dbccdbc",
+    "url": "/okta-vidispine/static/js/2.f80e8b0c.chunk.js"
+  },
+  {
+    "revision": "263a433bdd06faf5d48c4f71bce289e9",
+    "url": "/okta-vidispine/static/js/2.f80e8b0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fec02a902fdcc96a921",
+    "url": "/okta-vidispine/static/js/main.e422041e.chunk.js"
+  },
+  {
+    "revision": "0e82b01eec6b6aa636dc",
+    "url": "/okta-vidispine/static/js/runtime-main.4a00144e.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/okta-vidispine/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/okta-vidispine/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/okta-vidispine/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/okta-vidispine/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/okta-vidispine/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "7e72002067c9047c205fdb3521176196",
+    "url": "/okta-vidispine/static/media/checkbox-01.7e720020.png"
+  },
+  {
+    "revision": "d5554fe41aed23e1e49f249f2f6bcfaa",
+    "url": "/okta-vidispine/static/media/checkbox-01@2x.d5554fe4.png"
+  },
+  {
+    "revision": "c8c027005764d43d83566d88f53844c9",
+    "url": "/okta-vidispine/static/media/checkbox-sign-in-widget@2x.c8c02700.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/okta-vidispine/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/okta-vidispine/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/okta-vidispine/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/okta-vidispine/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/okta-vidispine/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/okta-vidispine/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "da5afed58d4a7ab9e44a16ee5387707b",
+    "url": "/okta-vidispine/static/media/loader@1x.da5afed5.gif"
+  },
+  {
+    "revision": "6225f3ca44b83090833064727a09cc95",
+    "url": "/okta-vidispine/static/media/montserrat-light-webfont.6225f3ca.woff"
+  },
+  {
+    "revision": "70df5da69722ec23336c227bf131d5ab",
+    "url": "/okta-vidispine/static/media/montserrat-light-webfont.70df5da6.ttf"
+  },
+  {
+    "revision": "8cde2a0e8a4a698a32a3b1c295b9fa8b",
+    "url": "/okta-vidispine/static/media/montserrat-light-webfont.8cde2a0e.svg"
+  },
+  {
+    "revision": "a11330db59d1e0d7b0935e86754b86be",
+    "url": "/okta-vidispine/static/media/montserrat-light-webfont.a11330db.eot"
+  },
+  {
+    "revision": "362ffe720ba40be359302cab7f83c517",
+    "url": "/okta-vidispine/static/media/montserrat-regular-webfont.362ffe72.eot"
+  },
+  {
+    "revision": "69eac499e9311b03ed69199e62ea962e",
+    "url": "/okta-vidispine/static/media/montserrat-regular-webfont.69eac499.ttf"
+  },
+  {
+    "revision": "6fedfc64a7bfbbb272c4e4d3b8597244",
+    "url": "/okta-vidispine/static/media/montserrat-regular-webfont.6fedfc64.svg"
+  },
+  {
+    "revision": "8f2822b73b5f9c106c6f2e0db820bcbb",
+    "url": "/okta-vidispine/static/media/montserrat-regular-webfont.8f2822b7.woff"
+  },
+  {
+    "revision": "432df6c2318e100c9bf4b820be0c66e6",
+    "url": "/okta-vidispine/static/media/okticon.432df6c2.svg"
+  },
+  {
+    "revision": "5e1f49dda77e01218444c76678856d3d",
+    "url": "/okta-vidispine/static/media/okticon.5e1f49dd.ttf"
+  },
+  {
+    "revision": "a560e17e7ab1ae0cc271da8cf0c62d8a",
+    "url": "/okta-vidispine/static/media/okticon.a560e17e.eot"
+  },
+  {
+    "revision": "db28723126138387cdf40680e6e0fa5d",
+    "url": "/okta-vidispine/static/media/okticon.db287231.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/okta-vidispine/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/okta-vidispine/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/okta-vidispine/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/okta-vidispine/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/okta-vidispine/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "c837a09b25ad85d682483da65dd6420e",
+    "url": "/okta-vidispine/static/media/radiobutton-01.c837a09b.png"
+  },
+  {
+    "revision": "dc47af40165cadf418a95b6a06964b2e",
+    "url": "/okta-vidispine/static/media/radiobutton-01@2x.dc47af40.png"
+  },
+  {
+    "revision": "eea28db2b98f4df40b5608a54d0b9131",
+    "url": "/okta-vidispine/static/media/sign-on-widget-spinner.eea28db2.gif"
+  },
+  {
+    "revision": "591cc98f43cbb68c10ddf345cb2d164e",
+    "url": "/okta-vidispine/static/media/u2f_usb.591cc98f.png"
+  },
+  {
+    "revision": "f10c9db1d9eb441f0bdaffce5bb672a1",
+    "url": "/okta-vidispine/static/media/yubikeyDemo.f10c9db1.png"
+  }
+]);
